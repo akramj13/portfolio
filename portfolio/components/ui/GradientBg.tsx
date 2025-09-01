d58c4dbf798cddacf4dc3a5cb99d17ab3c1e14be
akramj13 <125495000+akramj13@users.noreply.github.com>
2025-09-01 17:53:11 -0400

    Reset site

--- portfolio/components/ui/GradientBg.tsx
+++
@@ -1,7 +0,0 @@
-import dynamic from "next/dynamic";
-import { BackgroundGradientAnimationSSR } from "./GradientSSR";
-
-export const BackgroundGradientAnimation = dynamic(
-  () => import("./GradientSSR"),
-  { ssr: false }
-);
